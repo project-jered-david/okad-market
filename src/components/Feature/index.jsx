58fbfd7548fbc82dbd1ffb3fc72605d5b3e8efd9
david <adjelek.dav@gmail.com>
2022-07-27 11:10:00 -0400

    fix css

--- src/components/Feature/index.jsx
+++ src/components/Feature/index.jsx
@@ -1,30 +1,7 @@
 import CardPropriety from "../CardPropriety";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faArrowAltCircleLeft,
-    faArrowAltCircleRight,
-} from "@fortawesome/free-solid-svg-icons";
-/* import Carousel, { consts } from "react-elastic-carousel"; */
 import "./feature.css";
 import { proprieties } from "../../global/fake-proprieties.js";
-import styled from "styled-components";
-import { PRIMARY_COLOR } from "../../global/common-styles.js";
 import Slider from "react-slick";
-
-const Button = styled.button`
-    border: none;
-    color: ${PRIMARY_COLOR};
-    background-color: white;
-    &:disabled {
-        color: #e3e1e1;
-    }
-`;
-
-const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2 },
-    { width: 768, itemsToShow: 3, itemsToScroll: 3 },
-];
 
 const Feature = () => {
     const settings = {
@@ -32,16 +9,8 @@ const Feature = () => {
         speed: 600,
         slidesToShow: 3,
         slidesToScroll: 3,
+        initialSlide: 0,
         responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true,
-                },
-            },
             {
                 breakpoint: 680,
                 settings: {
